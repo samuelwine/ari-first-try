8ab462ec3f36a83958a933a1e80d9fdc44ef04d0
Shmuel Yaakov <shmuelyaakov@bluecare.express>
2022-02-13 15:34:51 +0000

    add console app

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Not associated
+//https://run.mocky.io/v3/0998fb1c-e38d-4799-a1a6-3cc240fae9d5
+//https://designer.mocky.io/manage/delete/0998fb1c-e38d-4799-a1a6-3cc240fae9d5/OtKgPpa76wOVx48itNAgJDlH3O4lXpCHgEHM
+
+//Associated but PIN not set on all accounts
+//https://run.mocky.io/v3/35f18658-c49e-4652-ba43-434a7f260e0d
+//https://designer.mocky.io/manage/delete/35f18658-c49e-4652-ba43-434a7f260e0d/LucWwWVHkNd27Hq3GQyXSU4dtX1LM003xMRZ
+
+//All set
+//https://run.mocky.io/v3/7b053044-6663-4c62-8592-5725c6220c12
+//https://designer.mocky.io/manage/delete/7b053044-6663-4c62-8592-5725c6220c12/RbKe9ENOtg5t5Gg8KJXlClvpOexhdpSJbd1D
+
+
+
