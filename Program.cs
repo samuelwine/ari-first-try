c2a1a12d04203a29d456f544948b5ff2be3c3eee
Shmuel <samuelwine@gmail.com>
2022-02-13 20:35:47 +0000

    extra functionality

--- Program.cs
+++ Program.cs
@@ -9,11 +9,13 @@
 //https://designer.mocky.io/manage/delete/35f18658-c49e-4652-ba43-434a7f260e0d/LucWwWVHkNd27Hq3GQyXSU4dtX1LM003xMRZ
 
 //All set
-//https://run.mocky.io/v3/7b053044-6663-4c62-8592-5725c6220c12
-//https://designer.mocky.io/manage/delete/7b053044-6663-4c62-8592-5725c6220c12/RbKe9ENOtg5t5Gg8KJXlClvpOexhdpSJbd1D
+//https://run.mocky.io/v3/dcd26e99-8b09-4461-b6d1-5ba77057edaf
+//https://designer.mocky.io/manage/delete/dcd26e99-8b09-4461-b6d1-5ba77057edaf/x3XryUZX0YximC6ps3A2vxcRNlnsR1fE3vW5
 
 using AsterNET.ARI;
 using AsterNET.ARI.Helpers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 string host = "135.181.204.69";
 int port = 8088;
@@ -29,13 +31,28 @@ client.OnStasisStartEvent += async (ac, se) =>
     ac.Channels.Answer(se.Channel.Id);
     HttpClient httpClient = new HttpClient();
     httpClient.DefaultRequestHeaders.Accept.Clear();
-    var json = await httpClient.GetAsync("https://run.mocky.io/v3/7b053044-6663-4c62-8592-5725c6220c12");
-    
-    SyncHelper.Wait(ac.Channels.Play(se.Channel.Id, "sound:moo2"), client);
+    var json = await httpClient.GetStringAsync("https://run.mocky.io/v3/35f18658-c49e-4652-ba43-434a7f260e0d");
+    var response = JsonSerializer.Deserialize<ResultObj>(json);
+    if(response.Pin == "8888")
+    {
+        SyncHelper.Wait(ac.Channels.Play(se.Channel.Id, "sound:moo2"), client);
+    }
+    else
+    {
+        SyncHelper.Wait(ac.Channels.Play(se.Channel.Id, "sound:you-wish-to-join"), client);
+    }
     ac.Channels.Hangup(se.Channel.Id);
 };
 client.Connect();
 Console.ReadKey();
+
+public class ResultObj
+{
+    [JsonPropertyName("pin")]
+    public string Pin { get; set; }
+    [JsonPropertyName("status")]
+    public string Status { get; set; }
+}
 
 
 
