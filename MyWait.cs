fc25baaf27b25e80bc420aea40233b4d59fb6acc
Shmuel Yaakov <shmuelyaakov@bluecare.express>
2022-03-01 18:56:12 +0000

    Add wiat functionality for multiple calls

---
+++ MyWait.cs
@@ -0,0 +1,24 @@
+﻿using AsterNET.ARI;
+using AsterNET.ARI.Models;
+
+namespace ari_first_try
+{
+    public static class SyncHelper
+    {
+        public static void MyWait(
+            this Playback playback,
+            IAriEventClient client)
+        {
+            AutoResetEvent playbackFinished = new AutoResetEvent(false);
+
+            client.OnPlaybackFinishedEvent += ((s, e) =>
+            {
+                if (playback.Id == e.Playback.Id)
+                    playbackFinished.Set();
+            });
+
+            playbackFinished.WaitOne();
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
-﻿using AsterNET.ARI;
-using AsterNET.ARI.Helpers;
+﻿using ari_first_try;
+using AsterNET.ARI;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 var config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
@@ -11,7 +9,7 @@ var config = new ConfigurationBuilder()
 
 string ip = config.GetValue<string>("aht_ari_ip");
 int port = config.GetValue<int>("aht_ari_port");
-string username = config.GetValue<string>("aht_ari_username"); 
+string username = config.GetValue<string>("aht_ari_username");
 string password = config.GetValue<string>("aht_ari_password");
 string application = config.GetValue<string>("aht_ari_application");
 
@@ -21,7 +19,7 @@ AriClient client = new AriClient(endpoint, application);
 client.OnStasisStartEvent += (ariClient, startEvent) =>
 {
     ariClient.Channels.Answer(startEvent.Channel.Id);
-    ariClient.Channels.Play(startEvent.Channel.Id, "sound:moo2").Wait(ariClient);
+    ariClient.Channels.Play(startEvent.Channel.Id, "sound:moo2").MyWait(ariClient);
     ariClient.Channels.Hangup(startEvent.Channel.Id);
 };
 
