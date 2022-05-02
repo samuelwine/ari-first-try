91a00745c02e3f53050dc487c95d8101cd17a79b
Shmuel Yaakov <shmuelyaakov@bluecare.express>
2022-05-02 19:00:29 +0100

    changes

--- Program.cs
+++ Program.cs
@@ -25,8 +25,8 @@ client.OnStasisStartEvent += (ariClient, startEvent) =>
         //ariClient.Channels.Play(startEvent.Channel.Id, "sound:moo2");
         //ariClient.Channels.Play(startEvent.Channel.Id, "sound:moo2");
         //ariClient.Channels.Play(startEvent.Channel.Id, "sound:moo2");
-
-        var externalMediaChannel = ariClient.Channels.ExternalMedia("hello-world", "192.168.8.122:12272", "g722");
+        var dict = new Dictionary<string, string> { { "JITTERBUFFER(adaptive)", "default" } };
+        var externalMediaChannel = ariClient.Channels.ExternalMedia("hello-world", "192.168.8.121:12272", "g722", null, dict, null, null, null, "in");
         var channelVars = externalMediaChannel.Channelvars as Dictionary<string, object>;
         var listeningPort = channelVars["UNICASTRTP_LOCAL_PORT"].ToString();
         var bridge = ariClient.Bridges.Create();
@@ -43,7 +43,10 @@ client.OnStasisStartEvent += (ariClient, startEvent) =>
         var ffmpegPath = "C:\\Users\\samue\\OneDrive\\Desktop\\ffmpeg.exe";
         //var ffmpegArgs = $"-re -i \"c:/users/samue/downloads/mynewstring.wav\" -c:a copy -f rtp -payload_type 9 \"rtp://192.168.8.122:17896\"";
 
-        var ffmpegArgs = $"-re -i \"c:/users/samue/downloads/mynewstring.g722\" -acodec g722 -filter:a \"atempo=0.5,atempo=0.5,atempo=0.5 \" -f rtp -payload_type 9 \"rtp://{ip}:{listeningPort}\"";
+        //var ffmpegArgs = $"-re -i \"C:\\Users\\samue\\Downloads\\shemaBeni.wav\" -f rtp -payload_type 0 \"rtp://192.168.8.122:17896\"";
+        //var ffmpegArgs = $"-loglevel 48 -readrate 1 -i \"C:\\Users\\samue\\Downloads\\counting.wav\" -packetsize 200 -f rtp -payload_type 0 \"rtp://{ip}:{listeningPort}\" ";
+        var ffmpegArgs = $"-loglevel 48 -re -i \"C:\\Users\\samue\\Downloads\\shemaBeni.wav\"  -f rtp -payload_type 0 \"rtp://{ip}:{listeningPort}\" ";
+        //var ffmpegArgs = $"-re -i \"c:/users/samue/downloads/mynewstring.g722\" -acodec g722 -filter:a \"atempo=0.5,atempo=0.5,atempo=0.5 \" -f rtp -payload_type 9 \"rtp://{ip}:{listeningPort}\"";
         var proc = System.Diagnostics.Process.Start(ffmpegPath, ffmpegArgs);
 
         //var ffmpeg = new Engine();
